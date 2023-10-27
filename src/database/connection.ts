b9458dd08c4d7012068a62528a910ba2f6170ef4
maik <estevan.maik@gmail.com>
2023-10-27 14:43:20 -0300

    feat: added docker-compose file, updated mongoose connection configs

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -6,22 +6,25 @@ dotenv.config({ path: '.env' });
 const uri = process.env.MONGO_URI || '';
 
 export const connectDB = async () => {
-	try {
-		const connection = await mongoose.connect(uri);
+  try {
+    const connection = await mongoose.connect(uri, {
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    });
 
-		console.log(`MongoDb connected: ${connection.connection.host}`);
-	} catch (e) {
-		console.log(e);
-		process.exit(1);
-	}
+    console.log(`MongoDb connected: ${connection.connection.host}`);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
 };
 
 export const testSetup = async () => {
-	await connectDB();
-	await dbSeed();
+  await connectDB();
+  await dbSeed();
 };
 
 export const disconnectDB = async () => {
-	await mongoose.connection.db.dropDatabase();
-	await mongoose.connection.close();
+  await mongoose.connection.db.dropDatabase();
+  await mongoose.connection.close();
 };
